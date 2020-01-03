fcdb78efc84eb3f0fc80b97e7e89ba3cef827155
Civoremo <omerovic_n@hotmail.com>
2020-01-03 16:36:25 -0500

    graphql boilerplate

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://bsl-api-59b4e8d7a4.herokuapp.com/backyard-sports-league-api/dev`
+});
+exports.prisma = new exports.Prisma();
